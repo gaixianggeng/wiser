4929933388a9cecd79970c457236e36698ebfda9
gaixianggeng <gaih@outlook.com>
2022-03-28 23:58:33 +0800

    new go project

--- wiser_go/internal/default.go
+++ wiser_go/internal/default.go
@@ -2,4 +2,19 @@ package internal
 
 //InvertedIndexValue 倒排索引
 type InvertedIndexValue struct {
+	TokenID       int64
+	postingList   *PostingsList
+	docsCount     int64
+	positionCount int64
+}
+
+// InvertedIndexHash 倒排hash
+type InvertedIndexHash map[int64]*InvertedIndexValue
+
+// PostingsList 倒排列表
+type PostingsList struct {
+	DocID         int64
+	positions     []int64
+	positionCount int64
+	next          *PostingsList
 }
