32e8ccd22f1faf1672bca3149910301f28f685a0
gaixianggeng <gaih@outlook.com>
2022-03-31 20:43:28 +0800

    add merge posting

--- wiser_go/internal/postings.go
+++ wiser_go/internal/postings.go
@@ -1,6 +1,7 @@
 package internal
 
 // MergePostings merge two postings list
+// https://leetcode-cn.com/problems/he-bing-liang-ge-pai-xu-de-lian-biao-lcof/
 func MergePostings(pa, pb *PostingsList) *PostingsList {
 	ret := new(PostingsList)
 	p := new(PostingsList)
@@ -29,4 +30,34 @@ func MergePostings(pa, pb *PostingsList) *PostingsList {
 	}
 
 	return ret.next
+}
+
+// MergeInvertedIndex 合并两个倒排索引
+func MergeInvertedIndex(base, toBeAdded *InvertedIndexHash) {
+	for tokenID, index := range *base {
+		if toBeAddedIndex, ok := (*toBeAdded)[tokenID]; ok {
+			index.postingList = MergePostings(index.postingList, toBeAddedIndex.postingList)
+			index.docsCount += toBeAddedIndex.docsCount
+			index.positionCount += toBeAddedIndex.positionCount
+			delete(*toBeAdded, tokenID)
+		}
+	}
+	for tokenID, index := range *toBeAdded {
+		(*base)[tokenID] = index
+	}
+
+	// inverted_index_value *p, *temp;
+
+	// HASH_ITER(hh, to_be_added, p, temp) {
+	//     inverted_index_value *t;
+	//     HASH_DEL(to_be_added, p);
+	//     HASH_FIND_INT(base, &p->token_id, t);
+	//     if (t) {
+	//         t->postings_list = merge_postings(t->postings_list, p->postings_list);
+	//         t->docs_count += p->docs_count;
+	//         free(p);
+	//     } else {
+	//         HASH_ADD_INT(base, token_id, p);
+	//     }
+	// }
 }
